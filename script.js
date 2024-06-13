bf064db0cf6f41edfa7c811b169c044f3ff1ecd6
Swag-blips <coderblip@gmail.com>
2024-06-13 01:13:04 +0100

    API works

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  //get important dom elements
+  let search = document.getElementById("search-input");
+  let play = document.getElementById("play");
+
+  // fetch the word using the searchValue as reference
+  const fetchWord = async function (value) {
+    try {
+      const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
+      const response = await fetch(url);
+
+      const data = await response.json();
+
+      const firstWord = data[0];
+
+      console.log(firstWord);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Test addeventlistener function
+  play.addEventListener("click", () => {
+    let searchValue = search.value.trim();
+
+    if (searchValue) {
+      fetchWord(searchValue);
+    } else {
+      console.log("Enter a valid word");
+    }
+  });
+});
