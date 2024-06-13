78f5c28e50c818c51a8441af6685d982bc2ff867
Swag-blips <coderblip@gmail.com>
2024-06-13 09:47:05 +0100

    code, updated

--- script.js
+++ script.js
@@ -4,27 +4,43 @@ document.addEventListener("DOMContentLoaded", () => {
   //get important dom elements
   let search = document.getElementById("search-input");
   let play = document.getElementById("play");
+  let mainText = document.getElementById("main-text");
+  let phonetics = document.getElementById("phonetics");
+  let definitionsContainer = document.getElementById("definitions");
+  let error = "";
 
   // fetch the word using the searchValue as reference
   const fetchWord = async function (value) {
     try {
       const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
       const response = await fetch(url);
-
       const data = await response.json();
 
-      const firstWord = data[0];
-
-      console.log(firstWord);
+      displayText(data);
     } catch (err) {
       console.log(err);
     }
   };
 
-  // Test addeventlistener function
+  // function to display text
+  const displayText = (value) => {
+    const mainWord = value[0];
+    mainText.innerText = mainWord.word;
+    phonetics.innerText = mainWord.phonetics[0].text;
+
+    definitionsContainer.innerHTML = "";
+    const definitions = mainWord.meanings[0].definitions.slice(0, 3);
+
+    definitions.forEach((definition, index) => {
+      let li = document.createElement("li");
+      li = `${definition}`;
+      definitionsContainer.appendChild(li);
+    });
+  };
+
+  // Test add eventlistener function
   play.addEventListener("click", () => {
     let searchValue = search.value.trim();
-
     if (searchValue) {
       fetchWord(searchValue);
     } else {
